55bc3e53f576733bf6b7052f026188d5c08ad2df
songyang1992 <39903647+songyang1992@users.noreply.github.com>
2020-03-28 21:37:10 +0800

    20200328

--- C_level2.c
+++ C_level2.c
@@ -736,3 +736,92 @@ void NONO ()
   fclose(rf) ;
   fclose(wf) ;
 }
+填空
+#include  <stdio.h>
+#define   N   3
+#define   M   4
+/**********found**********/
+void fun(int (*a)[N], int k)
+{ int i,temp ;
+/**********found**********/
+  for(i = 0 ; i < N ; i++)
+  { temp=a[0][i] ;
+/**********found**********/
+    a[0][i] = a[k][i] ;
+    a[k][i] = temp ;
+  }
+}
+void main()
+{ int x[M][N]={ {1,2,3},{4,5,6},{7,8,9},{10,11,12} },i,j;
+  printf("The array before moving:\n\n");
+  for(i=0; i<M; i++)
+  {  for(j=0; j<N; j++) printf("%3d",x[i][j]);
+     printf("\n\n");
+  }
+  fun(x,2);
+  printf("The array after moving:\n\n");
+  for(i=0; i<M; i++)
+  {  for(j=0; j<N; j++) printf("%3d",x[i][j]);
+     printf("\n\n");
+  }
+}
+填空
+#include  <string.h>
+#include  <stdio.h>
+void fun(char  t[])
+{
+ char c;
+ int i,j;
+ /*************found**************/
+ for(i=strlen(t)-1;i;i--)
+     for(j=0;j<i;j++)
+        /*************found**************/
+	  if(t[j]>t[j+1])
+          {
+           c= t[j];
+           t[j]=t[j+1];
+           t[j+1]=c;
+          }
+}
+void main()
+{
+ char  s[81];
+ 
+ printf("\nPlease  enter a character string :");
+ gets(s);
+ printf("\n\nBefore sorting :\n  %s",s);
+ fun(s);
+ printf("\nAfter sorting decendingly:\n %s",s);
+}
+编程
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void fun(char *ss)
+{
+  int i;
+  char *p = ss;
+  for (i=0; i<strlen(ss);i++)
+  {
+  if(p[i]>='A' && p[i] <= 'z'&& i%2 != 0)
+     p[i]=p[i]-32;
+  }
+}
+void main()
+{
+  FILE *wf;
+  char tt[81],s[81]="abc4Efg";
+  system("CLS");
+  printf("\nPlease enter an string within 80 characters:\n");
+  gets(tt);
+  printf("\n\nAfter changing, the string\n  %s",tt);
+  fun(tt);
+  printf("\nbecomes\n %s\n",tt);
+/******************************/
+  wf=fopen("out.dat","w");
+  fun(s);
+  fprintf (wf,"%s",s);
+  fclose(wf);
+/*****************************/
+}
